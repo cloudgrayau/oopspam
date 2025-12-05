df4df26f4cebba7dc22710eafc51bdea3fd1087d
Gijs Stegehuis <gijs@robuust.digital>
2025-12-05 11:41:46 +0100

    Add support for Formie nested fields

--- src/integrations/FormieIntegration.php
+++ src/integrations/FormieIntegration.php
@@ -1,11 +1,10 @@
 <?php
 namespace cloudgrayau\oopspam\integrations;
 use cloudgrayau\oopspam\OOPSpam;
-use Craft;
 use yii\base\Event;
 
 class FormieIntegration {
-  
+
   public $integration = '';
   public function getName(): string {
     return 'Formie';
@@ -13,38 +12,47 @@ class FormieIntegration {
 
   public function parse(string $integration): void {
     $this->integration = $integration;
+
     Event::on(\verbb\formie\services\Submissions::class, \verbb\formie\services\Submissions::EVENT_AFTER_SPAM_CHECK, function(\verbb\formie\events\SubmissionSpamCheckEvent $e){
-      $fields = 0;
+      $allFields = [];
       $params = [
-        'content' => []
+        'content' => [],
       ];
-      foreach($e->submission->form->getCustomFields() as $field){
-        switch(get_class($field)){
+
+      $this->extractFields($e->submission->form->getCustomFields(), $allFields);
+
+      foreach ($allFields as $field) {
+        switch (get_class($field)) {
           case 'verbb\formie\fields\formfields\Email':
           case 'verbb\formie\fields\Email':
-            $params['email'] = (string)$e->submission->getFieldValue($field->handle);
-            $fields++;
+            $params['email'] = (string) $e->submission->getFieldValue($field->getFieldKey());
             break;
           case 'verbb\formie\fields\formfields\MultiLineText':
           case 'verbb\formie\fields\MultiLineText':
-            $params['content'][] = (string)$e->submission->getFieldValue($field->handle);
-            $fields++;
+            $params['content'][] = (string) $e->submission->getFieldValue($field->getFieldKey());
             break;
         }
       }
       if ((OOPSpam::$plugin->settings->enableContextual) && (!empty(OOPSpam::$plugin->settings->contextualContent)) && (in_array($this->integration, OOPSpam::$plugin->settings->contextual))){
         $params['contextual'] = true;
       }
-      if ($fields > 0) {
-        if (!OOPSpam::$plugin->antiSpam->checkSpam($params, $this->getName())){
-          $e->submission->isSpam = true;
-        }
-      } else {
-        Craft::warning("No relevant fields found in Formie `{$e->submission->form->handle}` submission to check for spam.", 'oopspam');
+      if (!OOPSpam::$plugin->antiSpam->checkSpam($params, $this->getName())){
+        $e->submission->isSpam = true;
       }
     });
   }
-  
+
+  protected function extractFields($fields, &$allFields)
+  {
+    foreach ($fields as $field) {
+      $allFields[] = $field;
+
+      // Check if field acts as a container (Group, Repeater, Fieldset, etc.)
+      if (method_exists($field, 'getFields')) {
+          $this->extractFields($field->getFields(), $allFields);
+      }
+    }
+  }
 }
 
 ?>
